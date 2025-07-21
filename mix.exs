151c8c04aa7070ab793b95bef83af489a692b250
NSHkr <ZeroTrust@NSHkr.com>
2025-07-20 19:34:49 -1000

    cleanup

--- mix.exs
+++ mix.exs
@@ -44,7 +44,7 @@ defmodule Snakepit.MixProject do
       maintainers: ["NSHkr <ZeroTrust@NSHkr.com>"],
       links: %{"GitHub" => "https://github.com/nshkrdotcom/snakepit"},
       files: ~w(lib priv assets docs .formatter.exs mix.exs README* LICENSE* CHANGELOG* DIAGS*),
-      exclude_patterns: ["priv/plts", "priv/python/**/__pycache__", "priv/python/**/*.egg-info"]
+      exclude_patterns: ["priv/plts", "**/__pycache__", "**/*.egg-info"]
     ]
   end
 
