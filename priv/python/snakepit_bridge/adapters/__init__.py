40c8a17a473d2a9a6f709723fa68bc7548fcdba2
NSHkr <ZeroTrust@NSHkr.com>
2025-07-26 20:04:01 -1000

    initial migration to 3layer

--- priv/python/snakepit_bridge/adapters/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Snakepit Bridge Adapters
-
-This package contains various adapter implementations for different use cases.
-"""
-
-from .enhanced import EnhancedBridge
-
-__all__ = ["EnhancedBridge"]
